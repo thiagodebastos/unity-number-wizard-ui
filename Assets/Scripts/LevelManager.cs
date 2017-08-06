d8008fc503a99800a3e61dd3de412df0380de51b
Thiago de Bastos <thiago@thiagodebastos.com>
2017-08-06 23:41:06 +1000

    add level management to start button

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
 
-  // Use this for initialization
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+    }
 }
